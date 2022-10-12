8156c9114c818912031db6784fc0cc4283ac68d3
UTKARSH MOHAN <115492964+UTKARSHMOHAN2003@users.noreply.github.com>
2022-10-13 02:42:27 +0530

    Create TRANSPOSE OF MATRIX .C

---
+++ TRANSPOSE OF MATRIX .C
@@ -0,0 +1,45 @@
+#include <stdio.h>
+ 
+void main()
+{
+    static int array[10][10],transpose[10][10];
+    int i, j, m, n;
+ 
+    printf("Enter the order of the matrix \n");
+    scanf("%d %d", &m, &n);
+    printf("Enter the coefficients of the matrix\n");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            scanf("%d", &array[i][j]);
+        }
+    }
+    printf("The given matrix is \n");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            printf(" %d", array[i][j]);
+        }
+        printf("\n");
+    }
+    for (i = 0;i < m;i++)
+    {
+        for (j = 0; j < n; j++)
+        {   
+            transpose[j][i] = array[i][j];
+        }
+    }
+ 
+    // printing the transpose of a matrix
+    printf("Transpose of matrix is \n");
+    for (i = 0; i< n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+           printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+}
